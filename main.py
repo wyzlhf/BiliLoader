d7640c8a299f87d15411ae8498a6439e0a7ca8f9
wyzlhf <haifeng0130@126.com>
2022-12-20 18:01:02 +0800

    添加了根据文件标题包含的字符串删除文件功能，主要针对视频有多个语种甚至包含AI自动生成语种字幕，导致播放器中播放时需要手动设置字幕的问题。

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from subtitle_converter import convert_json_subtitle_to_srt_subtitle
 # pvl=PlaylistVideoLoader('BV1f3411J7w4')
 
 if __name__ == '__main__':
-    # BVID: str = 'BV1eD4y1e7KE'
-    # BVID: str = 'BV1ba411H7pR'
     BVID: str = 'BV1ha4y1a7hR'
 
     # 下载视频
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+from typing import List
+
 from subtitle_converter import SubtitleConverter
 
 
@@ -17,19 +19,32 @@ def rename_merged_video(old_path: str, new_path: str):
     else:
         print("The file does not exist")
 
-def change_json_subtitle_to_srt(path:str):
-    all_json_subtitle_file:list=[]
-    all_file:list=os.listdir(path)
-    file:str
+
+def change_json_subtitle_to_srt(path: str):
+    all_json_subtitle_file: list = []
+    all_file: list = os.listdir(path)
+    file: str
     for file in all_file:
-        file_name_list:list=file.split('.')
-        file_type=file_name_list[-1]
-        if file_type=='json':
+        file_name_list: list = file.split('.')
+        file_type = file_name_list[-1]
+        if file_type == 'json':
             all_json_subtitle_file.append(file)
     print(all_json_subtitle_file)
     for json_file in all_json_subtitle_file:
-        file_name=path+'\\'+json_file
-        sc=SubtitleConverter(file_name)
+        file_name = path + '\\' + json_file
+        sc = SubtitleConverter(file_name)
         sc.convert_subtitle()
+
+
+def delete_subtitle_with_string_in_title(path: str = './', string_in_title: str = '') -> None:
+    file_names_list: List[str] = os.listdir(path)
+    # print(file_names_list)
+    for file_name in file_names_list:
+        if string_in_title in file_name:
+            full_name: str = path + '\\' + file_name
+            os.remove(full_name)
+            print('成功删除文件：', file_name)
+
+
 if __name__ == '__main__':
-    change_json_subtitle_to_srt('D:\VIDEO\【Udemy付费课程】Node.js, Express, MongoDB & More The Complete Bootcamp 2022\P1【Udemy付费课程】Node.js, Express, MongoDB & More The Complete Bootcamp 2022（中英文字幕）')
+    delete_subtitle_with_string_in_title('D:\VIDEO\希勒【金融市场】【18课全双语高清】', '.en-US')
