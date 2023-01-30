a1a332e7f70c3addd9143c43da1e1ec421a2e4fc
wyzlhf <haifeng0130@126.com>
2023-01-30 12:23:30 +0800

    解决了视音频文件名称包含半角冒号（:）或|符号无法重命名，而导致不能下载的问题。

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from subtitle_converter import convert_json_subtitle_to_srt_subtitle
 # pvl=PlaylistVideoLoader('BV1f3411J7w4')
 
 if __name__ == '__main__':
-    BVID: str = 'BV1kE41157pa'  #Statistics 110: Probability 概率论 哈佛大学（中英）
     # BVID: str = 'BV1pL411p7wp'  #History of Ancient and Modern Philosophy (Spring 2018)
-    # BVID: str = 'BV1Tq4y1X77M'  #History of Analytic Philosophy (Spring 2021)
+    BVID: str = 'BV1Tq4y1X77M'  #History of Analytic Philosophy (Spring 2021)
     # BVID: str = 'BV1Ni4y1d7QW'  #【惠顿学院】西方哲学史-81讲-中英字幕可自选
     # 下载视频
     plv = PlaylistVideoLoader(BVID)
